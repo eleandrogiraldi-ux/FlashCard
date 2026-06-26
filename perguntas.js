f9340c7f4adf991a2688ab969b7d36575c580e75
eleandrogiraldi-ux <eleandro.giraldi@escola.pr.gov.br>
2026-06-26 14:19:39 -0300

    llllddsas

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'robótica',
+    'Quais são os três pilares fundamentais da robótica?',
+    ' Sensoriamento, Processamento e Ação. O robô primeiro coleta dados do ambiente (sensoriamento), depois analisa e toma decisões com base nesses dados (processamento), e por fim realiza um movimento físico ou tarefa (ação)'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'robótica',
+    ' Qual é a diferença prática entre um "sensor" e um "atuador"?',
+    'O sensor envia informações de fora para dentro do robô (como temperatura, distância ou luz). O atuador recebe ordens do processador e faz o robô se mover ou reagir (como motores, garras, rodas e pistões)'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'robótica',
+    'O que são os chamados "Cobots" e o que os diferencia dos tradicionais?',
+    ' Cobots são robôs colaborativos projetados para trabalhar lado a lado com humanos sem a necessidade de barreiras físicas de segurança. Ao contrário dos robôs industriais tradicionais (que são isolados em jaulas porque podem machucar alguém), os cobots possuem sensores de força que os fazem parar imediatamente se tocarem em uma pessoa.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'robótica',
+    '. Como um robô autônomo consegue desviar de obstáculos?',
+    'Ele utiliza o ciclo de leitura-mapeamento-desvio. Sensores (como ultrassônicos ou LiDAR) emitem ondas ou lasers para medir a distância de objetos ao redor. O código programado no robô calcula o tempo de retorno dessas ondas e muda a rota dos motores caso a distância seja menor do que o limite seguro',
 )
